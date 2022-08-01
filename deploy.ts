e46f3a58598117b6e1ff71b1abd56164e17da858
Juan Luis Caro <blademaker.live@gmail.com>
2022-08-01 01:52:48 +0000

    Fix favicon

--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,4 @@
-import { json, serve } from "https://deno.land/x/sift@0.3.3/mod.ts";
+import { json, serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
 import { getMeme } from "./api.ts";
 
 serve({
@@ -6,7 +6,7 @@ serve({
     const meme = await getMeme();
     return json(meme);
   },
-  "/favicon.icon": () => new Response("Not found"),
+  "/favicon.ico": () => json({ message: "Not found" }, { status: 404 }),
   "/:slug": async (
     _request: Record<string, unknown>,
     params: Record<string, unknown>,
@@ -15,5 +15,5 @@ serve({
     const meme = await getMeme(slug);
     return json(meme);
   },
-  404: () => new Response("Not found"),
+  404: () => json({ message: "Not found" }, { status: 404 }),
 });
