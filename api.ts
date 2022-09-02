bd3830589b335c165bb5e0db77c0fb821ac61010
Juan Luis Caro <blademaker.live@gmail.com>
2022-09-02 02:27:45 +0200

    Update api.ts

--- api.ts
+++ api.ts
@@ -3,8 +3,8 @@ import { buildMeme } from "./utils.ts";
 
 const baseURL = "https://www.reddit.com/r/{subreddit}/hot/.json?count=100";
 
-export async function getMeme(subreddit?: string): Promise<Meme> {
-  if (!subreddit) subreddit = "memes";
-  const url = baseURL.replace("{subreddit}", subreddit);
+export async function getMeme(r?: string): Promise<Meme> {
+  if (!r) r = "memes";
+  const url = baseURL.replace("{subreddit}", r);
   return await buildMeme(url);
 }
